afd6f9933929c2525e5742d943163fb4b034942f
Momo Momo <Kuan.Lee.Taiwan@gmail.com>
2022-02-13 16:01:07 -0700

    Rebuild some columns in data

--- Salaryproject.py
+++ Salaryproject.py
@@ -17,9 +17,9 @@ df.to_csv('glassdoor.csv')
 
 df = pd.read_csv('glassdoor.csv',header=0)
 df = df.iloc[:,1:]
-df['EstimatedSalary'] = df['Salary'].replace('$','').apply(lambda x: x.split('/')[0])
-df['EstimatedSalary'] = df['EstimatedSalary'].apply(lambda x: x.replace('$','').replace(',',''))
-#Salary
+df['Salary'] = df['Salary'].replace('$','').apply(lambda x: x.split('/')[0])
+df['Salary'] = df['Salary'].apply(lambda x: x.replace('$','').replace(',',''))
+# Function
 def SalaryTrans(x):
     try:
         x = float(x)
@@ -29,12 +29,7 @@ def SalaryTrans(x):
             return x * 40 *4 *12
     except:
         return(np.nan)
-    
-df['EstimatedSalary'] = df['EstimatedSalary'].apply(lambda x: SalaryTrans(x) )
-df.dropna(subset=['EstimatedSalary'],inplace=True)
-#average working hours a week is 40 hrs, sorucing from google
 
-#Title
 def TitleTran(x):
     if 'analyst' in x.lower() or 'business' in x.lower() :
         return('Data Analyst')
@@ -47,15 +42,41 @@ def TitleTran(x):
 
 def seniority(x):
     if ('sr' in x.lower()) or ('senior' in x.lower()) or ('principal' in x.lower()) or ('lead' in x.lower()) or ('staff' in x.lower()):
-        return '1''
+        return '1'
     elif ('jr' in x.lower()) or ('junior' in x.lower()):
         return '0'
     else:
         return 'none'
     
-df['Title_Trans'] = df['Title'].apply(lambda x: TitleTran(x))   
+def Size(x):
+    if x[0] == '1':
+        return '10000+'
+    elif x == 'NoSize':
+        return x
+    elif x == 'Unknown':
+        return 'NoSize'
+    else:
+        return str((int(x.split()[0]) + int(x.split()[2]))/2)
+    
+    
+df['Salary'] = df['Salary'].apply(lambda x: SalaryTrans(x) )
+df.dropna(subset=['Salary'],inplace=True)
+#average working hours a week is 40 hrs, sorucing from google
+ 
+df['Title'] = df['Title'].apply(lambda x: TitleTran(x))   
 df['Seniority']= df['Title'].apply(lambda x: seniority(x))
+df['Location'] = df['Location'].apply(lambda x: x.split(',')[1] if x != 'Remote' else 'Remote')
+df['Size'] = df['Size'].apply(lambda x : Size(x))
+df['Revenue'] = df['Revenue'].replace('$','').apply(lambda x: 'NoRevenue' if x[0]=="U" else x)
 
+# Dscrb
+df['Python'] = df["Dscrb"].apply(lambda x: 1 if 'python' in x.lower() else 0)
+df['R'] = df["Dscrb"].apply(lambda x: 1 if 'r' in x.lower() else 0)
+df['SQL'] = df["Dscrb"].apply(lambda x: 1 if 'sql' in x.lower() else 0)
+df['Spark'] = df["Dscrb"].apply(lambda x: 1 if 'spark' in x.lower() else 0)
+df['Aws'] = df["Dscrb"].apply(lambda x: 1 if 'aws' in x.lower() else 0)
+df['Excel'] = df["Dscrb"].apply(lambda x: 1 if 'excel' in x.lower() else 0)
+df['PowerBI'] = df["Dscrb"].apply(lambda x: 1 if 'powerbi' in x.lower() else 0)
+df['Tableau'] = df["Dscrb"].apply(lambda x: 1 if 'tableau' in x.lower() else 0)
 
-
-
+df
